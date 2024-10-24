43a88f945ffcc1d75e5e78377943462f315306e3
Saisha Verma <saisha.verma@qsstechnosoft.com>
2024-10-24 14:11:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res) => {
+  res.send("Hey <3");
+});
+
 app.use("/test", (req, res) => {
   res.send("I love you 3000 <3");
 });
